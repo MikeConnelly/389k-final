7e8f7d0247de78d3674a3b7c1d3e39240e26c91d
Michael Connelly <mrc33@atlanticbb.net>
2020-05-12 19:09:17 -0400

    different mongo version url

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ var Comment = require('./models/Comment');
 var getTags = require('./utils/getTags');
 
 dotenv.config();
-mongoose.connect(`mongodb+srv://Mike389k:${process.env.MONGOPASS}@cluster0-hfvx4.mongodb.net/test?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb://Mike389k:${process.env.MONGOPASS}@cluster0-shard-00-00-hfvx4.mongodb.net:27017,cluster0-shard-00-01-hfvx4.mongodb.net:27017,cluster0-shard-00-02-hfvx4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-mongoose.connection.on('error', () => {
+mongoose.connection.once('open', function(){
+  console.log('Mongo Connected!');
+}).on('error', () => {
   console.log('MongoDB connection error');
   process.exit(1);
 });
